bf492a2526a06c812c0de4eea18f792b80c8b89a
TheeAnalyst_ke <ziondaily11@gmail.com>
2026-06-26 16:23:52 +0300

    correct streamplit option menu import

--- Safaricom Data Project.py
+++ Safaricom Data Project.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 import plotly_express as px
 import plotly.graph_objects as go
-from streamlit_option_menu import streamlit_option_menu
+from streamlit_option_menu import option_menu
 from pathlib import Path  
 import warnings
 import sys 
