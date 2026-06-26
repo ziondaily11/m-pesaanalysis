81d28c41acd74b227f60a0ef38ee84d835e5d5fc
TheeAnalyst_ke <ziondaily11@gmail.com>
2026-06-26 17:59:55 +0300

    v

--- Safaricom Data Project.py
+++ Safaricom Data Project.py
@@ -20,11 +20,15 @@ def get_image_base64(path):
 logo = get_image_base64("SAF-MAIN-LOGO.png")  
 st.markdown(f"""
     <div style="display:flex; align-items:center; gap:10px; margin-bottom:1rem;">
-    <img src="data:image/png;base64,{logo}" width="220" 
-         style="vertical-align:middle; margin-top:0; display:block;">
-    <h2 style="color:#288C1D; font-family:'Courier New', monospace; 
-               font-size:38px; margin:0; line-height:1;">M-Pesa Analysis</h2>
-</div>
+        <div style="display:flex; align-items:center;">
+            <img src="data:image/png;base64,{logo}" 
+                 style="width:220px; height:auto; object-fit:contain;">
+        </div>
+        <div style="display:flex; align-items:center;">
+            <span style="color:#288C1D; font-family:'Courier New', monospace; 
+                       font-size:38px; margin:0; line-height:1;">M-Pesa Analysis</span>
+        </div>
+    </div>
 """, unsafe_allow_html=True)
 st.set_page_config(
     page_title= "m-pesa Analysis",
