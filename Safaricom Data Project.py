64d0f19b97015fb308c8fae8f8b31c74811f0de3
TheeAnalyst_ke <ziondaily11@gmail.com>
2026-06-27 20:15:47 +0300

    g

--- Safaricom Data Project.py
+++ Safaricom Data Project.py
@@ -257,6 +257,7 @@ def show_home():
     )
     fraud_rate_bar.update_layout(
         height= 300,
+        margin= dict(t= 40, b= 10, l= 10, r= 10),
         yaxis= dict(
             ticksuffix= "%",
             title= None,
