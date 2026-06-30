01fe4616eae3e002e90d9b411b8679ee507e83a9
TheeAnalyst_ke <ziondaily11@gmail.com>
2026-06-30 09:11:06 +0300

    v

--- Safaricom Data Project.py
+++ Safaricom Data Project.py
@@ -483,6 +483,6 @@ def show_home():
     
     with st.container(border= True):
         st.plotly_chart(fig)
-        st.caption("The yaxes have been zoomed in to improve visualisation")
+        st.caption("NOTE:The yaxes have been zoomed in to improve visualisation")
 
 show_home()
