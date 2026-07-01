0a5e3f5398274a76cffb6b2c3d14d3d4d4568ceb
TheeAnalyst_ke <ziondaily11@gmail.com>
2026-07-01 12:30:19 +0300

    c

--- Safaricom Data Project.py
+++ Safaricom Data Project.py
@@ -213,17 +213,19 @@ def show_home():
         }
         </style>
         """, unsafe_allow_html=True)
-    col1, col2 = st.columns([0.3, 2.7], vertical_alignment= "center", gap="small")
+    
     lef, mid_lef, mid, mid_righ, rig, col = st.columns(6)
-    with col1:
-        st.image(Path(__file__).parent /"projectlogo.png", width= 140)
-    with col2:
-        st.markdown("""
-                <h1 style="color: #288C1D; font-family: Courier New, monospace; 
-                        font-size:30px">
-                    FINPULSE REPORT
-                </h1>
-            """, unsafe_allow_html=True)
+    with st.container(border= True):
+            col1, col2 = st.columns([0.3, 2.7], vertical_alignment= "center", gap="small")
+                    with col1:
+                        st.image(Path(__file__).parent /"projectlogo.png", width= 140)
+                    with col2:
+                        st.markdown("""
+                                <h1 style="color: #288C1D; font-family: Courier New, monospace; 
+                                        font-size:30px">
+                                    FINPULSE REPORT
+                                </h1>
+                            """, unsafe_allow_html=True)
     
     with lef:
         st.metric(label= "Total Transactions", 
